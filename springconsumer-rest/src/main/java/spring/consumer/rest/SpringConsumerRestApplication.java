d91e3e643a2ae6e694e11e495c62524460315fde
Yasser Alejandro Palacios <ypalac01@mail.bbk.ac.uk>
2018-04-29 17:46:17 +0200

    Spring consumer rest example
    Spring consumer rest example

---
+++ springconsumer-rest/src/main/java/spring/consumer/rest/SpringConsumerRestApplication.java
@@ -0,0 +1,52 @@
+package spring.consumer.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
+
+/*
+ * Source: https://spring.io/guides/gs/consuming-rest/
+ * 
+ * 
+ */
+
+@SpringBootApplication
+public class SpringConsumerRestApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringConsumerRestApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsumerRestApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+		return args -> {
+			Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random",Quote.class);
+			log.info(quote.toString());
+		};
+	}
+
+	/*
+	public static void main(String[] args) {
+		RestTemplate restTemplate = new RestTemplate();
+		Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random",Quote.class);
+		log.info(quote.toString());
+		
+		//SpringApplication.run(SpringConsumerRestApplication.class, args);
+
+	}
+	*/
+	
+}
